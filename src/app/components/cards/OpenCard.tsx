78142fff49528ee74612850cd07b9428de53d00c
yoonucho <tomorrowcho@gmail.com>
2024-05-14 14:55:09 +0900

    Update OpenCard component with new image and logo styles

--- src/app/components/cards/OpenCard.tsx
+++ src/app/components/cards/OpenCard.tsx
@@ -15,11 +15,10 @@ export default function OpenCard() {
 	});
 
 	return (
-		//  <div className={`${styles.container}`}>
 		<div className={`${styles.container} ballonRise`}>
 			<audio ref={audioRef} src="/sounds/card-rise.wav" />
 			<Logo />
-			<Image src="/assets/images/open-card.svg" alt="open card" width={300} height={300} />
+			<Image src="/assets/images/open-card.png" alt="open card" width={300} height={300} />
 		</div>
 	);
 }
