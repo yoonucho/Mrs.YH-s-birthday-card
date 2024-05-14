a787650b18a71970452d2cbad21e685065feb0aa
yoonucho <tomorrowcho@gmail.com>
2024-05-14 18:38:09 +0900

    Add SEO settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,19 +3,19 @@ import "../styles/globals.css";
 
 export const metadata: Metadata = {
 	title: {
-		template: " | %s",
-		default: "",
+		template: "정여사님의 생신을 축하합니다 | %s",
+		default: "정여사님의 축하 카드 페이지",
 	},
 	description: "",
 	icons: {
 		icon: "/favicon.ico",
 	},
 	openGraph: {
-		title: "",
+		title: "정여사님의 생신을 축하합니다",
 		description: "",
-		siteName: "",
-		images: { url: "" },
-		url: "",
+		siteName: "정여사님의 생신을 축하합니다",
+		images: { url: "https://mrs-yh-s-birthday-card.vercel.app/assets/images/yu-open-graph.png" },
+		url: "https://mrs-yh-s-birthday-card.vercel.app",
 		type: "website",
 	},
 };
