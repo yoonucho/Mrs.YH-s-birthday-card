974c7ef210292b4768a7a467f7e51f0696bf85cf
yoonucho <tomorrowcho@gmail.com>
2024-05-15 16:55:47 +0900

    Update not-found module CSS and import path

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import styles from "../styles/not-found.module.css";
+import styles from "./not-found.module.css";
 import { useRouter } from "next/navigation";
 
 export default function NotFound() {
@@ -8,10 +8,13 @@ export default function NotFound() {
 	const handleClick = () => {
 		router.back();
 	};
+
 	return (
 		<main className={styles.container}>
 			<Image src="/assets/images/message-icon.svg" alt="not found" width={500} height={500} />
 			<h1 className={styles.errorText}>요청하신 페이지가 존재하지 않습니다.</h1>
+			<button className={styles.backBtn} onClick={handleClick}>
+				Home으로 가기</button>
 		</main>
 	);
 }
