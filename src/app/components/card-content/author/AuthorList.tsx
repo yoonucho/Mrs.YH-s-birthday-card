cc82b03ca759c6869a5e30596c2eebc318d0832b
yoonucho <tomorrowcho@gmail.com>
2024-05-15 15:29:44 +0900

    Update author list with attribution links and styles

--- src/app/components/card-content/author/AuthorList.tsx
+++ src/app/components/card-content/author/AuthorList.tsx
@@ -15,10 +15,36 @@ export default function AuthorList() {
 					의
 				</span>
 				<div>
-					<span>하트 아이콘 클릭 시 효과음 : elmasmalo1</span>
-					<span>카드 클릭 시 효과음: jomse</span>
-					<span>카드 위로 날아가는 효과음: raaghav81</span>
-					<span>생일축하곡: SergeQuadrado</span>
+					<span>
+						하트 아이콘 클릭 시 효과음 : elmasmalo1
+						<a href="https://freesound.org/people/elmasmalo1/sounds/376968" target="_blank">
+							elmasmalo1
+						</a>
+					</span>
+					<span>
+						카드 클릭 시 효과음:{" "}
+						<a href="https://freesound.org/people/jomse/sounds/428652" target="_blank">
+							jomse
+						</a>
+					</span>
+					<span>
+						카드 위로 날아가는 효과음:{" "}
+						<a href="https://freesound.org/people/raaghav81/sounds/277234" target="_blank">
+							raaghav81
+						</a>
+					</span>
+					<span>
+						축하 효과음:{" "}
+						<a href="https://freesound.org/people/theuned/sounds/391869" target="_blank">
+							theuned
+						</a>
+					</span>
+					<span>
+						생일축하곡:{" "}
+						<a href="https://freesound.org/people/SergeQuadrado/sounds/646117" target="_blank">
+							SergeQuadrado
+						</a>
+					</span>
 				</div>
 				<span className={styles.imgAuthor}>배경이미지 원작자</span>
 				<div>
@@ -32,6 +58,14 @@ export default function AuthorList() {
 						Adi Goldstein
 					</a>
 				</div>
+
+				<span className={styles.imgAuthor}>이미지 & Youtube 영상 원작자</span>
+				<div>
+					<a href="https://www.youtube.com/@CHOSK_INFP" target="_blank">
+						CHOSK
+					</a>
+				</div>
+
 				<p>
 					<strong>감사합니다!</strong>{" "}
 				</p>
